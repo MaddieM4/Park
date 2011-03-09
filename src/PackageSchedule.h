e9bcd6f175c7d95dc4cab7477ad7e07365ef272e
Philip Horger <campadrenalin@gmail.com>
2011-03-08 18:06:48 -0800

    A bit of fiddling with the schedule spec

--- src/PackageSchedule.h
+++ src/PackageSchedule.h
@@ -6,6 +6,12 @@ using std::string
 
 class PackageSchedule {
 	string text;
+
+	PackageSchedule::PackageSchedule(string text);
+	PackageSchedule::next();
+	int PackageSchedule::make(string command);
+	int PackageSchedule::kill();
+	PackageSchedule::operator=(PackageSchedule* newschedule);
 }
 
 #endif // PACKAGE_SCHEDULE
