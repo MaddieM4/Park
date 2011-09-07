dfcbe093e95ba2ba40196380bd4f3e37853fb8e6
Philip Horger <campadrenalin@gmail.com>
2011-09-07 02:10:20 -0700

    Continuing to hack away at C in all its awfulness

--- src/park-meta.c
+++ src/park-meta.c
@@ -9,20 +9,29 @@ typedef enum {
 	rpm
 } PackageType;
 
+typedef struct {
+	char *name;
+} config;
+
+// define some functions
+config* defaultyaml();
+mergeyaml(config*, config*);
+
 int main(int argc, char *argv[]){
 	PackageType ptype;
 
 	if (argc < 3) {
+		if (argc == 2) {
+			if (!strcmp(argv[1],"--help")) superusage();
+		}
 		usage("Not enough arguments");
 	}
 	if (!strcmp(argv[1],"D")) {
 		ptype = deb;
-	} else if (argv[1]=="S") {
+	} else if (!strcmp(argv[1],"S")) {
 		ptype = slack;
-	} else if (argv[1]=="R") {
+	} else if (!strcmp(argv[1],"R")) {
 		ptype = rpm;
-	} else if (argv[1]=="--help") {
-		superusage();
 	} else {
 		usage("Bad package type argument");
 	}
@@ -30,12 +39,48 @@ int main(int argc, char *argv[]){
 	char* dir = argv[argc-1];
 	printf(dir);
 
-	yaml_parser_t parser;
-	yaml_event_t event;
+	//char* pfname, sfname;
 
-	//yaml_parser_set_input_file(&parser, 
+	config *primary, *secondary;
+	primary = defaultyaml();
+	secondary = defaultyaml();
+
+	mergeyaml(primary, secondary);
+
 	return 0;
 }
+
+config *defaultyaml() {
+	config* dyaml = malloc(sizeof(config));
+	dyaml->name = "hellodolly";
+	return dyaml;
+}
+
+mergeyaml(config* primary, config* secondary) {
+	if (secondary->name != NULL) primary->name = secondary->name;
+}
+
+printyaml(config* tbp) {
+	yaml_emitter_t emitter;
+	yaml_event_t event;
+
+	//yaml_emitter_initialize(&emitter);
+
+	//yaml_emitter_set_output_file(&emitter, stdout);
+
+	int write_handler() {
+		return 0;
+	}
+	//yaml_set_output(&emitter, write_handler
+}
+
+//config *readyaml(char* filename) {
+//	FILE *fp;
+//	fp = fopen(filename, "rb");
+//	if (fp == null) {
+//		return defaultyaml();
+//	}
+//}
 
 usage(char* specific) {
 	fprintf(stderr, "park-meta: %s\nSyntax: park-meta {D | S | R } [-e] dir\n", specific);
@@ -43,6 +88,15 @@ usage(char* specific) {
 }
 
 superusage(){
-	printf("");
+	printf("\
+park-meta TYPE [-e] DIR\n\
+\n\
+Generates the metadata for park-build to make a package of type TYPE\n\
+If the -e flag is present, export results to environment variables\n\
+If not, print merged YAML to stdout\n\
+DIR is the root dir of the package, of which there must be a .park-meta\n\
+folder, or else this program will have to make assumptions\n\
+\n\
+Type-specific config properties override package defaults.\n");
 	exit(0);
 }
